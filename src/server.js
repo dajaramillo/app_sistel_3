aa110822512296060de29be665c389cf6f771edc
Daniel <dajaramillo@unicauca.edu.co>
2024-09-04 15:19:33 -0500

    Creamos base de datos MongoDB

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ const path = require('path');
 
 
 //configuraciones
-const app = express()
+const app = express();
+require('./database');
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs.engine({
