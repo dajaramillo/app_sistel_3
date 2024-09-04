b023f20c95a9252b6e3118020e4596c161094b87
Daniel <dajaramillo@unicauca.edu.co>
2024-09-04 14:51:54 -0500

    Subiendo nuevamente app

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.get('/login', (req, res) => {
+    res.render('../views/login');
+});
+
+router.get('/register', (req, res) => {
+    res.render('../views/register');
+});
+
+module.exports = router;
