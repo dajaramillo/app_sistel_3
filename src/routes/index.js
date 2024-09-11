cfd301e19a57718c60d0b438133aa697eec1df21
Daniel <dajaramillo@unicauca.edu.co>
2024-09-10 22:27:02 -0500

    movemos server.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,10 @@ router.get('/login', (req, res) => {
     res.render('../views/login');
 });
 
+router.get('/', (req, res) => {
+    res.render('../views/login');
+});
+
 router.get('/register', (req, res) => {
     res.render('../views/register');
 });
